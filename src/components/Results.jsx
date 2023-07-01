fca23561a9e9ab7f63969cd21b71da09b38a9b52
Athenkosi Vinqi <nervadevelop@gmail.com>
2023-07-01 16:18:12 +0200

    highlighting champs

--- src/components/Results.jsx
+++ src/components/Results.jsx
@@ -43,11 +43,30 @@ const Results = () => {
    getChampions(currentYear.value);
   }, [currentYear]);
 
+  console.log(curYear.getFullYear())
+
+
   return (
     <div id='standings' className='section'>
      <div className='container h-auto mx-auto max-w-[90%] flex flex-col justify-start items-start'>
      <h1 className="text-3xl font-semibold py-5" > {currentYear.value} Standings
-</h1> 
+     
+</h1>
+<div className='py-5'>
+<div className='flex flex-row gap-2'>
+<div>      <p className='text-sm'>Season Winner</p>
+</div>
+      <div className='bg-green-600 h-[20px] w-[20px] rounded-[100%]'></div>
+      
+     </div> 
+     <div className='flex flex-row gap-2'>
+<div>      <p className='text-sm'>Results May Change</p>
+</div>
+      <div className='bg-orange-600 h-[20px] w-[20px] rounded-[100%]'></div>
+      
+     </div> 
+</div>
+
      <div className='flex flex-col gap-5 w-full'>
 <div>
 
@@ -64,6 +83,7 @@ const Results = () => {
  (
               <table className='w-full shadow-lg rounded-lg table-auto'>
                 <thead >
+
                   <tr>
                     <th className='text-left py-7 px-1 lg:px-7'>POS</th>
                     <th className='text-left py-7 px-1 lg:px-7'>DRIVER</th>
@@ -74,7 +94,8 @@ const Results = () => {
                 </thead>
                 <tbody className='divide-y'>
                   {season.map((item, index) => (
-                    <tr key={index}>
+                    
+                    <tr key={index} className={item?.position == 1 && currentYear.value < curYear.getFullYear() ? 'bg-green-600' : item?.position == 1 && currentYear.value >= curYear.getFullYear() ? 'bg-orange-600' :  'bg-transparent'  }>
                   
                 
                       <td className='py-7 px-1 lg:px-7'>
@@ -89,7 +110,6 @@ const Results = () => {
               </table>
             )}
  </div>
-
 
      </div>
     </div>

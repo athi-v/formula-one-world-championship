2dc70dd91eb79fbdcac9ebd4bb85e6afe148a637
Athenkosi Vinqi <nervadevelop@gmail.com>
2023-07-01 14:04:47 +0200

    updated links to 404 page

--- src/pages/ErrorPage.jsx
+++ src/pages/ErrorPage.jsx
@@ -11,7 +11,7 @@ const goHome = () => {
      <div className='container h-[100vh] mx-auto max-w-[90%] flex justify-center items-center'>
      <div className='flex flex-col justify-center items-center gap-5'>
      <h1 className='text-7xl font-bold'>404</h1>
-     <p className='text-md'>Ooopsie...</p> 
+     <p className='text-md'>Page not found...</p> 
 <button onClick={goHome} className='flex px-5 py-2 bg-red-600  items-center gap-4  text-white font-semilbold hover:text-black hover:bg-transparent hover:border-[1px] hover:border-grey'>
     <div>Return Home</div>
 </button>

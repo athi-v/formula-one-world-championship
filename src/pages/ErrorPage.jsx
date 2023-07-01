b1c289755348500a4b1df089a19ec5c9c85ad754
Athenkosi Vinqi <nervadevelop@gmail.com>
2023-07-01 14:01:23 +0200

    updated 404 page

--- src/pages/ErrorPage.jsx
+++ src/pages/ErrorPage.jsx
@@ -12,7 +12,7 @@ const goHome = () => {
      <div className='flex flex-col justify-center items-center gap-5'>
      <h1 className='text-7xl font-bold'>404</h1>
      <p className='text-md'>Ooopsie...</p> 
-<button onClick={goHome} className='flex px-5 py-2 bg-teal-400 rounded'>
+<button onClick={goHome} className='flex px-5 py-2 bg-red-600  items-center gap-4  text-white font-semilbold hover:text-black hover:bg-transparent hover:border-[1px] hover:border-grey'>
     <div>Return Home</div>
 </button>
 
